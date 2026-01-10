e8f0c8779defc643cf1dba404e99701e5678d616
Mukhtar Mohamed <toojywasoney360@gmail.com>
2026-01-10 11:13:34 +0800

    Feedback Module + CSS in the html pages

--- src/main/java/com/secj3303/model/Feedback.java
+++ src/main/java/com/secj3303/model/Feedback.java
@@ -1,13 +1,6 @@
 package com.secj3303.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "feedback")
@@ -16,10 +9,28 @@ public class Feedback {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 250)
     private String comment;
 
     @ManyToOne
     @JoinColumn(name = "person_id", nullable = false)
     private Person person;
+
+    // Constructors
+    public Feedback() {}
+
+    public Feedback(String comment, Person person) {
+        this.comment = comment;
+        this.person = person;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
+
+    public Person getPerson() { return person; }
+    public void setPerson(Person person) { this.person = person; }
 }
