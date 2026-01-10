f1e69fe32db19e647031af5721e0799c09e33a4d
Hamza Zacaria <hamzaUTM2005@gmail.com>
2026-01-10 11:49:11 +0800

    Chat added no css, contain time stamp, user message and username.

--- src/main/java/com/secj3303/security/SecurityConfig.java
+++ src/main/java/com/secj3303/security/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +41,7 @@ public class SecurityConfig {
 
             .authorizeHttpRequests()
                 .requestMatchers("/login", "/register", "/css/**").permitAll()
+                .requestMatchers("/chat/**").hasAnyRole("MEMBER", "ADMIN","PROFESSIONAL")
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/member/**").hasRole("MEMBER")
                 .requestMatchers("/professional/**").hasRole("PROFESSIONAL")
